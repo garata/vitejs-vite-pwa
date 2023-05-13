6408c50e88637816b4aae8e2a8c3ea201c358daa
garata <giorgio.arata@consultant.com>
2023-05-13 23:47:12 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-pwa
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-nzp4zy)
---
+++ vite.config.js
@@ -0,0 +1,38 @@
+import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    VitePWA({
+      manifest: {
+        icons: [
+          {
+            src: '/icons/512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: ({ url }) => {
+              return url.pathname.startsWith('/api');
+            },
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'api-cache',
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+            },
+          },
+        ],
+      },
+    }),
+  ],
+});
